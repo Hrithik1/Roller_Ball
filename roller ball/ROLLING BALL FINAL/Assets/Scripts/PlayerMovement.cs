c8ff65df2113a8522dd02acdc4f105926faeb102
Hrithik1 <40459883+Hrithik1@users.noreply.github.com>
2019-11-12 11:58:11 +0530

    Level Design

---
+++ roller ball/ROLLING BALL FINAL/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forward;
+    public float horizontal = 10f;
+    public float vertical = 10f;
+    public float up = 4f;
+    protected bool jump;
+    protected Joystick joystick;
+    protected joybutton joybutton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<joybutton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0f, 0f, forward);
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(joystick.Horizontal * horizontal,
+            rigidbody.velocity.y, joystick.Vertical * vertical);
+
+        if (!jump && joybutton.Pressed)
+        {
+            jump = true;
+            rigidbody.velocity += Vector3.up * up;
+        }
+        if (jump && !joybutton.Pressed)
+        {
+            jump = false;
+
+        }
+    }
+}
